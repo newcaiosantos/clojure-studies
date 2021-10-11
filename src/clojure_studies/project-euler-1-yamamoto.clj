e2e62b78f67ad4cd1b5bcd2e329dadbb78d94045
Caio Santos <newcaiosantos@gmail.com>
2021-10-10 22:33:19 -0300

    add single line version for project-euler-1-yamamoto

--- src/clojure_studies/project-euler-1-yamamoto.clj
+++ src/clojure_studies/project-euler-1-yamamoto.clj
@@ -3,3 +3,5 @@
   (or (= 0 (rem x 3)) (= 0 (rem x 5))))
 
 (reduce + (filter pega-multiplos (range 1000)))
+
+; single line version => (reduce + (filter #(or (= 0 (rem % 3)) (= 0 (rem % 5))) (range 1000)))
