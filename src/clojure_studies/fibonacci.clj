de3171d607273477af6079d5383d8aad61963b89
Caio Santos <newcaiosantos@gmail.com>
2021-10-11 15:23:45 -0300

    formatting

--- src/clojure_studies/fibonacci.clj
+++ src/clojure_studies/fibonacci.clj
@@ -1,3 +1,4 @@
+; fibonacci using function overloading
 (defn fi
   ([max seq]
    (let [[a b] (reverse seq)
@@ -7,13 +8,7 @@
        (fi max (into seq [next])))))
   ([max] (fi max [0 1 1])))
 
-; sum of even fibo numbers until 4.000.000
-(def res (reduce + (filter #(= 0(rem % 2)) (fi 4000000))))
-
-(println res)
-
-
-
+; fibonacci using loop/recur
 (defn fib
   [max]
   (loop [seq [0 1 1]]
@@ -23,4 +18,5 @@
         seq
         (recur (into seq [next]))))))
 
-(println (fib 10))
+(println (str "(fi 1000): " (fi 1000)))
+(println (str "(fib 1000): " (fib 1000)))
